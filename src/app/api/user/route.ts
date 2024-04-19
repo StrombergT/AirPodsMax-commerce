47a70cac4a96cbb160481f6320068c7ff451648c
StrombergT <tpr.stromberg@gmail.com>
2024-04-19 15:47:09 +0200

    api integration with sign-up

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,11 +1,21 @@
 import { db } from "@/src/lib/db";
 import { NextResponse } from "next/server";
 import { hash } from "bcrypt";
+import * as z from "zod";
+
+const userSchema = z.object({
+  username: z.string().min(1, "Username is required").max(100),
+  email: z.string().min(1, "Email is required").email("Invalid email"),
+  password: z
+    .string()
+    .min(1, "Password is required")
+    .min(8, "Password must have at least 8 characters"),
+});
 
 export async function POST(req: Request) {
   try {
     const body = await req.json();
-    const { email, username, password } = body;
+    const { email, username, password } = userSchema.parse(body);
 
     const existingUserByEmail = await db.user.findUnique({
       where: { email: email },
