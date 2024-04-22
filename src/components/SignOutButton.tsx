3c92379b08a3096cdf46ed50bda288a2eda9a338
StrombergT <tpr.stromberg@gmail.com>
2024-04-22 10:17:50 +0200

    sessionprovider and signout component

---
+++ src/components/SignOutButton.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { signOut } from "next-auth/react";
+import { Button } from "./ui/button";
+
+const SignOutButton = () => {
+  return (
+    <div className="flex gap-2">
+      <Button
+        onClick={() =>
+          signOut({
+            redirect: true,
+            callbackUrl: `${window.location.origin}/sign-in`,
+          })
+        }
+        variant={"destructive"}
+      >
+        Sign Out
+      </Button>
+    </div>
+  );
+};
+
+export default SignOutButton;
