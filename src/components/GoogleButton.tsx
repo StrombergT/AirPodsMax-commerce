13109fdaa32d0b8ba76e231348b2e4b2fe726217
StrombergT <tpr.stromberg@gmail.com>
2024-05-15 12:59:06 +0200

    implemented Google login

--- src/components/GoogleButton.tsx
+++ src/components/GoogleButton.tsx
@@ -1,18 +1,42 @@
-import { FC, ReactNode } from "react";
+import { ReactNode, useState } from "react";
 import { Button } from "./ui/button";
+import { signIn } from "next-auth/react";
 
-interface GoogleButtonProps {
+type GoogleButtonProps = {
   children: ReactNode;
-}
+};
 
-const GoogleButton: FC<GoogleButtonProps> = ({ children }) => {
-  const loginGoogle = () => console.log("log in with google");
+export default function GoogleButton({ children }: GoogleButtonProps) {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const loginGoogle = async () => {
+    try {
+      setIsLoading(true);
+      await signIn("google", { callbackUrl: "http://localhost:3000" });
+    } catch (err) {
+      setIsLoading(false);
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
-    <Button onClick={loginGoogle} className="w-full ">
+    <Button disabled={isLoading} onClick={loginGoogle} className="w-full ">
+      {isLoading && (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          className="h4 w-4 mr-2 animate-spin"
+        >
+          <path d="M21 12a9 9 0 1 1-6.219-8.56" />
+        </svg>
+      )}
       {children}
     </Button>
   );
-};
-
-export default GoogleButton;
+}
